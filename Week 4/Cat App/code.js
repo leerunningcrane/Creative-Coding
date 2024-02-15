30d84e8f2c997bf73c2dff5ef3343cff680cbc10
leerunningcrane <157534886+leerunningcrane@users.noreply.github.com>
2024-02-15 15:41:55 -0700

    week 4

---
+++ Week 4/Cat App/code.js
@@ -0,0 +1,28 @@
+onEvent("beginbutton", "click", function( ) {
+  playSound("assets/category_animals/cat.mp3", true);
+  setScreen("screen2");
+});
+onEvent("screen2", "click", function( ) {
+  setScreen("screen3");
+});
+onEvent("screen3", "click", function( ) {
+  setScreen("screen4");
+});
+onEvent("screen4", "click", function( ) {
+  setScreen("screen5");
+});
+onEvent("screen5", "click", function( ) {
+  setScreen("screen6");
+});
+onEvent("screen6", "click", function( ) {
+  setScreen("screen7");
+});
+onEvent("screen7", "click", function( ) {
+  setScreen("screen8");
+});
+onEvent("screen8", "click", function( ) {
+  setScreen("screen9");
+});
+onEvent("screen9", "click", function( ) {
+  setScreen("screen10");
+});
